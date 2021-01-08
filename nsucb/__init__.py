dee80fb564e4046f441984dc0cb48806ae8a5c62
MaxenceGiraud <maxence9giraud@gmail.com>
2021-01-08 18:42:34 +0100

    [Fix] Discounted UCB makes use of past rewards

--- nsucb/__init__.py
+++ nsucb/__init__.py
@@ -1,2 +1,3 @@
 from .ucb import UCB
 from .discounted_ucb import DiscountedUCB
+from .sliding_ucb import SlidingUCB
--- nsucb/discounted_ucb.py
+++ nsucb/discounted_ucb.py
@@ -9,8 +9,6 @@ class DiscountedUCB(UCB):
     ----------
     nbArms :int,
         Number of arms of bandit
-    alpha : float,
-        ..
     gamma : float,
         Discount factor. Must be between 0 and 1
     B : float,
@@ -18,10 +16,9 @@ class DiscountedUCB(UCB):
     Xi : float,
         Some appropritate constante that scale the discounted padding function
     '''
-    def __init__(self,nbArms,alpha=2,gamma=0.99,B=1,xi=1):
+    def __init__(self,nbArms,gamma=0.99,B=1,xi=1):
         assert gamma <=1 and gamma >= 0, "Gamma must be in the [0,1] interval"
         self.nbArms = nbArms
-        self.alpha = alpha
         self.gamma = gamma
         self.B = B
         self.xi = xi
@@ -30,6 +27,7 @@ class DiscountedUCB(UCB):
     def clear(self):
         self.history = [] # history of played arms, of size t
         self.history_bool = None # # history of played arms as booleans, of size (t,nbArms)
+        self.reward_history = []
         super().clear()
 
     def chooseArmToPlay(self):
@@ -38,7 +36,7 @@ class DiscountedUCB(UCB):
         else : 
             discount = np.ones(self.t)*self.gamma**(self.t-np.arange(self.t))
             N = np.array([np.sum(discount[np.where(i==self.history,1,0)]) for i in range(self.nbArms)])
-            X = 1/N * np.sum(discount.reshape(-1,1) * self.history_bool,axis=0) # discounted empirical average
+            X = 1/N * np.sum(discount.reshape(-1,1) *np.reshape(self.reward_history,(-1,1))* self.history_bool,axis=0) # discounted empirical average
 
             c = 2 * self.B * np.sqrt((self.xi * np.log(N.sum())/N))# discounted padding function   
 
@@ -46,6 +44,7 @@ class DiscountedUCB(UCB):
         
     def receiveReward(self,arm,reward):
         self.history.append(arm)
+        self.reward_history.append(reward)
         arm_bool = np.zeros(self.nbArms)
         arm_bool[arm] = 1
         
